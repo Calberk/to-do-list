670ad63a47b9b403706e1b2f8fadc353702dd660
Calberk <park.edmund@gmail.com>
2018-11-02 17:56:22 -0700

    Initial project setup complete

---
+++ src/dummy_data/list.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        title: 'Go to store',
+        details: 'Get eggs, bread, milk, and Coke',
+        _id: '001',
+    },
+    {
+        title: 'Finish Prototypes',
+        details: 'Do PHP, Node, React Prototypes by Sunday',
+        _id: '002',
+    },
+    {
+        title: 'Birthday Party',
+        details: 'Go to Ryan\'s 5th bday party on Saturday',
+        _id: '003',
+    },
+    {
+        title: 'Eat Dinner',
+        details: 'Take time out around 6-7pm to eat food',
+        _id: '004',
+    },
+    {
+        title: 'Sleep',
+        details: 'Go to sleep at night to get rest',
+        _id: '005',
+    }
+    
+    
+]
