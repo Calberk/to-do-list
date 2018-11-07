a868836c7b554a9acf46d3a95c31ed31e1aca019
Calberk <park.edmund@gmail.com>
2018-11-06 17:15:40 -0800

    Completed add item functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize';
+import '../assets/css/app.css';
 import List from './list'
+import AddItem from './add_item';
+import listData from '../dummy_data/list'
+import {randomString} from '../helpers'
 
 
-const App = () => (
-    <div className="container">
-        <h1 className="center">To Do List</h1>
-        <List/>
-    </div>
-);
+class App extends Component{
+    constructor (props){
+        super(props);
+
+        this.state = {
+            list: [],
+        }
+    }
+
+    getListData(){   //CALL SERVER TO GET DATA
+        this.setState({
+            list: listData
+        })
+    }
+
+    componentDidMount(){
+        this.getListData();
+    }
+
+    addItem = (item) => {
+        item._id = randomString(8);
+        this.setState({
+            list: [item, ...this.state.list]
+        })
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <h1 className="center">To Do List</h1>
+        
+                <AddItem add={this.addItem}/>  
+                <List data={this.state.list}/>
+            </div>
+        );
+    }
+}
+    
 
 export default App;
--- src/components/list.js
+++ src/components/list.js
@@ -1,29 +1,10 @@
 import React, {Component} from 'react';
-import listData from '../dummy_data/list'
+
 
 class List extends Component{
-    constructor (props){
-        super(props);
-
-        this.state = {
-            list: [],
-        }
-    }
-
-    getListData(){   //CALL SERVER TO GET DATA
-        this.setState({
-            list: listData
-        })
-    }
-
-    componentDidMount(){
-        this.getListData();
-    }
-
     render(){
-        console.log(this.state);
 
-        const listElements = this.state.list.map((item, index) => {
+        const listElements = this.props.data.map((item, index) => {
             return <li className="collection-item" key={item._id}>{item.title}</li>
         });
 
